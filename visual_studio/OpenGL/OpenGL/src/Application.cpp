379f924777764a8e919ad823480816be9edfe521
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-03-12 22:13:44 -0700

    video 4: glVertexAttributePoints

--- visual_studio/OpenGL/OpenGL/src/Application.cpp
+++ visual_studio/OpenGL/OpenGL/src/Application.cpp
@@ -37,6 +37,8 @@ int main(void)
 	glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // position
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
