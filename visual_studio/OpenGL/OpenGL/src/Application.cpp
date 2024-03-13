20b6c2166bbc4dadb789ff8dfdb4862bc00db1d0
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-03-13 15:05:34 -0700

    rename buffer to vbo

--- visual_studio/OpenGL/OpenGL/src/Application.cpp
+++ visual_studio/OpenGL/OpenGL/src/Application.cpp
@@ -105,12 +105,12 @@ int main(void)
     GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
 
-    unsigned int buffer;
-	GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    unsigned int vbo;
+	GLCall(glGenBuffers(1, &vbo));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
     GLCall(glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(float), positions, GL_STATIC_DRAW));
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0)); // position
+    GLCall(glEnableVertexAttribArray(0)); 
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0)); // position // this links the buffer to the currently bound Vertex Array (vao right now)
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 
     unsigned int ibo; // index buffer object
