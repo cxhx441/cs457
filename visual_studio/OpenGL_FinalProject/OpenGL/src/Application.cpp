04237ee1f1cd3fa83f917da4f6c99369d82f18e3
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-03-27 16:23:41 -0700

    remove old axes comments

--- visual_studio/OpenGL_FinalProject/OpenGL/src/Application.cpp
+++ visual_studio/OpenGL_FinalProject/OpenGL/src/Application.cpp
@@ -167,7 +167,6 @@ int main(void)
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 		GLCall(glBlendEquation(GL_FUNC_ADD)); // default, but being explicit
 
-		//initAxes();
 		initSand();
 
 		// axes
@@ -398,89 +397,6 @@ int main(void)
     return 0;
 }
 
-
-//void Axes( float length )
-//{
-//
-//	static float xx[ ] = { 0.f, 1.f, 0.f, 1.f };
-//	static float xy[ ] = { -.5f, .5f, .5f, -.5f };
-//	static int xorder[ ] = { 1, 2, -3, 4 };
-//	static float yx[ ] = { 0.f, 0.f, -.5f, .5f };
-//	static float yy[ ] = { 0.f, .6f, 1.f, 1.f };
-//	static int yorder[ ] = { 1, 2, 3, -2, 4 };
-//	static float zx[ ] = { 1.f, 0.f, 1.f, 0.f, .25f, .75f };
-//	static float zy[ ] = { .5f, .5f, -.5f, -.5f, 0.f, 0.f };
-//	static int zorder[ ] = { 1, 2, 3, 4, -5, 6 };
-//	const float LENFRAC = 0.10f; // fraction of the length to use as height of the characters:
-//	const float BASEFRAC = 1.10f; // fraction of length to use as start location of the characters:
-//
-//	glBegin( GL_LINE_STRIP );
-//		glVertex3f( length, 0., 0. );
-//		glVertex3f( 0., 0., 0. );
-//		glVertex3f( 0., length, 0. );
-//	glEnd( );
-//	glBegin( GL_LINE_STRIP );
-//		glVertex3f( 0., 0., 0. );
-//		glVertex3f( 0., 0., length );
-//	glEnd( );
-//	float fact = LENFRAC * length;
-//	float base = BASEFRAC * length;
-//
-//	glBegin( GL_LINE_STRIP );
-//	std::cout << "X" << std::endl;
-//		for( int i = 0; i < 4; i++ )
-//		{
-//			int j = xorder[i];
-//			if( j < 0 )
-//			{
-//
-//				glEnd( );
-//				glBegin( GL_LINE_STRIP );
-//				j = -j;
-//			}
-//			j--;
-//			glVertex3f( base + fact*xx[j], fact*xy[j], 0.0 );
-//			std::cout << base + fact*xx[j] << ", " << fact * xy[j] << ", " << 0.0 << std::endl;
-//		}
-//	glEnd( );
-//
-//	glBegin( GL_LINE_STRIP );
-//	std::cout << "Y" << std::endl;
-//		for( int i = 0; i < 5; i++ )
-//		{
-//			int j = yorder[i];
-//			if( j < 0 )
-//			{
-//
-//				glEnd( );
-//				glBegin( GL_LINE_STRIP );
-//				j = -j;
-//			}
-//			j--;
-//			glVertex3f( fact*yx[j], base + fact*yy[j], 0.0 );
-//			std::cout << fact*yx[j] << ", " << base + fact*yy[j] << ", " << 0.0 << std::endl;
-//		}
-//	glEnd( );
-//
-//	glBegin( GL_LINE_STRIP );
-//	std::cout << "Z" << std::endl;
-//		for( int i = 0; i < 6; i++ )
-//		{
-//			int j = zorder[i];
-//			if( j < 0 )
-//			{
-//
-//				glEnd( );
-//				glBegin( GL_LINE_STRIP );
-//				j = -j;
-//			}
-//			j--;
-//			glVertex3f( 0.0, fact*zy[j], base + fact*zx[j] );
-//			std::cout << 0.0 << ", " << fact*zy[j] << ", " << base + fact*zx[j] << std::endl;
-//		}
-//	glEnd( );
-//
-//}
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) 
 {
@@ -761,11 +677,3 @@ void initSand() {
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 8, colSSbo);
 }
 
-//void initAxes() {
-//	AxesList = glGenLists( 1 );
-//	glNewList( AxesList, GL_COMPILE );
-//		glLineWidth( 3 );
-//			Axes( 1.f );
-//		glLineWidth( 1. );
-//	glEndList( );
-//}
